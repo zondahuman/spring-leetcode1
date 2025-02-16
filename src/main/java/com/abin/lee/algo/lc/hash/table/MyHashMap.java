cf0b7b752171895e03c3fadd0449eeebcf7bd9c5
zondahuman <zondahuman@gmail.com>
2025-02-16 15:13:48 +0800

    add

--- src/main/java/com/abin/lee/algo/lc/hash/table/MyHashMap.java
+++ src/main/java/com/abin/lee/algo/lc/hash/table/MyHashMap.java
@@ -6,14 +6,28 @@ public class MyHashMap<K ,V> {
 
     private Object[] table;
 
+    // 增/改，复杂度 O(1)
     public void put(K key, V value){
         int index = hash(key) ;
         table[index] = value ;
     }
 
-    
+    // 查，复杂度 O(1)
+    public V get(K key){
+        int index = hash(key);
+        V value = (V)table[index];
+        return value;
+    }
+
+    // 删，复杂度 O(1)
+    public void remove(K key){
+        int index = hash(key);
+        table[index] = null;
+    }
 
 
+    // 哈希函数，把 key 转化成 table 中的合法索引
+    // 时间复杂度必须是 O(1)，才能保证上述方法的复杂度都是 O(1)
     public int hash(K key){
         return Objects.hashCode(key);
     }

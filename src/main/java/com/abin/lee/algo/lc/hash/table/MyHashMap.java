b0a5681591c19bd6e51a237d1561a63fd44d9650
zondahuman <zondahuman@gmail.com>
2025-02-16 15:02:38 +0800

    add

--- src/main/java/com/abin/lee/algo/lc/hash/table/MyHashMap.java
+++ src/main/java/com/abin/lee/algo/lc/hash/table/MyHashMap.java
@@ -1,15 +1,22 @@
 package com.abin.lee.algo.lc.hash.table;
 
+import java.util.Objects;
+
 public class MyHashMap<K ,V> {
-    
+
     private Object[] table;
 
     public void put(K key, V value){
-
+        int index = hash(key) ;
+        table[index] = value ;
     }
 
+    
 
 
+    public int hash(K key){
+        return Objects.hashCode(key);
+    }
 
 
 }

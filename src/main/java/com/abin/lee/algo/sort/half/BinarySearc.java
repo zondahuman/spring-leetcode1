5a153d3b6dfcc486f7f04c9d0919c508ce928fa1
zondahuman <zondahuman@gmail.com>
2025-02-21 01:07:10 +0800

    add

--- src/main/java/com/abin/lee/algo/sort/half/BinarySearc.java
+++ src/main/java/com/abin/lee/algo/sort/half/BinarySearc.java
@@ -28,16 +28,29 @@ public class BinarySearc {
 
 
 
-    public int binarySearchRecursion(int[] array, int target){
+    public int binarySearchLoop(int[] array, int target){
         int low = 0;
         int high = array.length-1;
         while(low <= high){
             int mid =low + (high-low)/2;
-            if(array[mid] > ){
-
+            if(array[mid] > target){
+                high = mid - 1;
+            }else if(array[mid] < target){
+                low = mid +1;
+            }else{
+                return mid;
             }
         }
 
-        return 0;
+        return -1;
     }
+
+    @Test
+    public void testLoop(){
+        int[] arr1 = new int[]{1,2,3,4,5};
+        int result = binarySearchLoop(arr1, 4);
+        System.out.println("result="+result);
+    }
+
+
 }

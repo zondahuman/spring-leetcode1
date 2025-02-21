e0b90238b701503f04476d328200ea9085ff7f1d
zondahuman <zondahuman@gmail.com>
2025-02-21 22:16:47 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/list/OneNineRemoveNthNodeFromEndofList.java
@@ -0,0 +1,38 @@
+package com.abin.lee.algo.leetcode.questions.list;
+
+/**
+ * 19. Remove Nth Node From End of List	19. 删除链表的倒数第 N 个结点
+ * https://leetcode.cn/problems/remove-nth-node-from-end-of-list/description/
+ */
+public class OneNineRemoveNthNodeFromEndofList {
+
+    // Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode first = head;
+        ListNode second = dummy;
+
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        while(null != first){
+            first = first.next ;
+            second = second.next ;
+        }
+        second.next = second.next.next ;
+        return dummy.next;
+    }
+
+
+}

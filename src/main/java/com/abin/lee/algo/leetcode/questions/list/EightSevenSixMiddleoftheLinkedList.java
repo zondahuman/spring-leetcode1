af26b239a89c1703fd39c37c613db4e72ee8f852
zondahuman <zondahuman@gmail.com>
2025-02-23 15:07:29 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/list/EightSevenSixMiddleoftheLinkedList.java
@@ -0,0 +1,33 @@
+package com.abin.lee.algo.leetcode.questions.list;
+
+/**
+ * 876. Middle of the Linked List	876. 链表的中间结点
+ * https://leetcode.cn/problems/middle-of-the-linked-list/description/
+ */
+public class EightSevenSixMiddleoftheLinkedList {
+    // Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        if(null == head)
+            return null;
+        ListNode slow = head ;
+        ListNode fast = head ;
+        while(fast != null && fast.next != null){
+            slow = slow.next ;
+            fast = fast.next.next;
+        }
+        return null;
+    }
+
+
+
+}

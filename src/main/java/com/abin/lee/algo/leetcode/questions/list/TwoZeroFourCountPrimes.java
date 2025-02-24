e6512f151abfeb4b2a2ecf88d215aba8e726d4c4
zondahuman <zondahuman@gmail.com>
2025-02-24 21:43:05 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/list/TwoZeroFourCountPrimes.java
@@ -0,0 +1,37 @@
+package com.abin.lee.algo.leetcode.questions.list;
+
+/**
+ * 204. 计数质数
+ * https://leetcode.cn/problems/count-primes/description/
+ * 204. Count Primes
+ * https://leetcode.com/problems/count-primes/description/
+ */
+public class TwoZeroFourCountPrimes {
+    // Definition for singly-linked list.
+    class ListNode {
+        int val;
+        TwoThreeMergekSortedLists.ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    /**
+     * 质因数，也叫素因数或质因子，是指能整除给定正整数的质数。质数是指只能被1和自身整除的正整数
+     * 质数：只有1和它本身两个因数（约数），那么这样的数叫做质数。比如7，只有1和7两个约数。
+     * 合数：除了能被1和它本身整除，还能被其他的正整数整除，那么这样的数叫做合数。比如8，有1、2、4和8四个约数。
+     * @param n
+     * @return
+     */
+    public int countPrimes(int n) {
+
+        return 0;
+    }
+
+
+
+
+
+}

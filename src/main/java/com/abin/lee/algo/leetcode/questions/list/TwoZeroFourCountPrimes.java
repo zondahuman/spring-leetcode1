d25392b01ad845ccf256c3882104b1b4e163a5d7
zondahuman <zondahuman@gmail.com>
2025-02-25 11:23:52 +0800

    add

--- src/main/java/com/abin/lee/algo/leetcode/questions/list/TwoZeroFourCountPrimes.java
+++ src/main/java/com/abin/lee/algo/leetcode/questions/list/TwoZeroFourCountPrimes.java
@@ -1,10 +1,16 @@
 package com.abin.lee.algo.leetcode.questions.list;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * 204. 计数质数
  * https://leetcode.cn/problems/count-primes/description/
  * 204. Count Primes
  * https://leetcode.com/problems/count-primes/description/
+ *
+ * https://labuladong.online/algo/frequency-interview/print-prime-number/
  */
 public class TwoZeroFourCountPrimes {
     // Definition for singly-linked list.
@@ -26,12 +32,53 @@ public class TwoZeroFourCountPrimes {
      * @return
      */
     public int countPrimes(int n) {
-
-        return 0;
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime , true);
+        for (int i = 2; i <n ; i++) {
+            if(isPrime[i]){
+                for (int j = i; j <n ; j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i <n ; i++) {
+            if(isPrime[i]){
+                count++;
+            }
+        }
+        return count;
     }
 
 
 
+    @Test
+    public void test1(){
+        int count = 10 ;
+//        int result = countPrimes(count) ;
+        int result = countPrimesLa(count) ;
+    }
+
+
+
+    public int countPrimesLa(int n) {
+        boolean[] isPrime = new boolean[n];
+        // 将数组都初始化为 true
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                // i 的倍数不可能是素数了
+                for (int j = 2 * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        return count;
+    }
 
 
 }

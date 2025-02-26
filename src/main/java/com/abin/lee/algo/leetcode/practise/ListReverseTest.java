5a10a346e39ac4827fcad9138421f0022792436a
zondahuman <zondahuman@gmail.com>
2025-02-26 22:13:17 +0800

    add

--- src/main/java/com/abin/lee/algo/leetcode/practise/ListReverseTest.java
+++ src/main/java/com/abin/lee/algo/leetcode/practise/ListReverseTest.java
@@ -1,5 +1,6 @@
 package com.abin.lee.algo.leetcode.practise;
 
+import com.abin.lee.algo.leetcode.questions.list.ReverseLinkedList206;
 import com.abin.lee.algo.util.JsonUtil;
 import org.junit.Test;
 
@@ -25,6 +26,30 @@ public class ListReverseTest {
         }
         return pre ;
     }
+
+    /**
+     * 反转链表前 N 个节点
+     * https://labuladong.online/algo/data-structure/reverse-linked-list-recursion/
+     * @param head
+     * @param n
+     * @return
+     */
+    ListNode reverseN(ListNode head, int n){
+        if(null == head || head.next == null)
+            return head ;
+        ListNode pre, cur, nxt;
+        pre = null; cur = head; nxt = head.next;
+        while(null != nxt && n > 0){
+            cur.next = pre ;
+            pre = cur ;
+            cur = nxt ;
+            nxt = nxt.next ;
+            n--;
+        }
+        head.next = cur;
+        return  head;
+    }
+
     
     @Test
     public void test1() {
@@ -51,12 +76,26 @@ public class ListReverseTest {
         l13.next = l14;
 
         ListNode listNode = reverseList(l1);
-
         listNode.next.next = listNode ;
-
         System.out.println("listNode=" + JsonUtil.toJson(listNode));
     }
 
 
+    @Test
+    public void test3() {
+        ListNode l1 = new ListNode(1);
+        ListNode l12 = new ListNode(2);
+        ListNode l13 = new ListNode(3);
+        ListNode l14 = new ListNode(4);
+        ListNode l15 = new ListNode(5);
+        l1.next = l12;
+        l12.next = l13;
+        l13.next = l14;
+        l14.next = l15;
+
+//        ListNode listNode = reverseList(l1);
+        ListNode listNode = reverseN(l1, 3);
+        System.out.println("listNode=" + JsonUtil.toJson(listNode));
+    }
 
 }

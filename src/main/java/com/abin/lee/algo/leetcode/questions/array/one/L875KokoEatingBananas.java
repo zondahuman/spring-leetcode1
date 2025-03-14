5cceb91efeb82a67aaf70b106cdfdc1067c0a3d9
zondahuman <zondahuman@gmail.com>
2025-03-14 12:19:03 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/array/one/L875KokoEatingBananas.java
@@ -0,0 +1,25 @@
+package com.abin.lee.algo.leetcode.questions.array.one;
+
+/**
+ * 875. 爱吃香蕉的珂珂
+ * https://leetcode.cn/problems/koko-eating-bananas/description/
+ * https://labuladong.online/algo/frequency-interview/binary-search-in-action/
+ */
+public class L875KokoEatingBananas {
+    /**
+     * 珂珂喜欢吃香蕉。这里有 n 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 h 小时后回来。
+     * 珂珂可以决定她吃香蕉的速度 k （单位：根/小时）。每个小时，她将会选择一堆香蕉，从中吃掉 k 根。如果这堆香蕉少于 k 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+     * 珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+     * 返回她可以在 h 小时内吃掉所有香蕉的最小速度 k（k 为整数）。
+     * @param piles
+     * @param h
+     * @return
+     */
+    public int minEatingSpeed(int[] piles, int h) {
+
+        return 0;
+    }
+
+
+
+}

bd709457c0348f2cb62c8d85a407cc3e29e03ec4
zondahuman <zondahuman@gmail.com>
2025-03-17 23:14:59 +0800

    add

--- src/test/java/com/abin/lee/test/algo/leetcode/temp/RenameMarch.java
+++ src/test/java/com/abin/lee/test/algo/leetcode/temp/RenameMarch.java
@@ -1,11 +1,16 @@
 package com.abin.lee.test.algo.leetcode.temp;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class RenameMarch {
 
     public static void main(String[] args) {
         // 指定要查找的目录路径
         String directoryPath = "/Users/lee/work"; // 请替换为你的实际目录路径
+        String newDirectoryPath = "/Users/lee/work/"; // 请替换为你的实际目录路径
 
         // 使用File类表示目录
         File directory = new File(directoryPath);
@@ -19,10 +24,22 @@ public class RenameMarch {
                 for (File file : files) {
                     // 检查是否为文件
                     if (file.isFile()) {
+                        String newFileName = "0"+file.getName();
+//                        File newFile = new File(newDirectoryPath + newFileName);
                         // 打印文件路径
-                        System.out.println("Found file: " + file.getAbsolutePath());
-                        System.out.println("Found file name: " + file.getName());
+                        System.out.println("Found file AbsolutePath: " + file.getAbsolutePath());
+//                        System.out.println("Found file name: " + file.getName());
                         // 你可以在这里添加更多逻辑来处理找到的文件
+                        // 4. 检查重命名操作是否成功
+                        Path sourcePath = Paths.get(file.getAbsolutePath());
+                        Path destinationPath = Paths.get(newDirectoryPath + newFileName);
+
+                        try {
+                            Files.copy(sourcePath, destinationPath);
+                            System.out.println("文件复制成功！源文件为:"+sourcePath.toString()+",修改后的文件为："+destinationPath.toString());
+                        } catch (IOException e) {
+                            System.err.println("文件复制失败：" + e.getMessage());
+                        }
                     }
                 }
             } else {

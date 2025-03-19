cbcea958d4019b13861452e47b38aea76ec7986b
zondahuman <zondahuman@gmail.com>
2025-03-19 20:33:08 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/tree/binary/L100SameTree.java
@@ -0,0 +1,83 @@
+package com.abin.lee.algo.leetcode.questions.tree.binary;
+
+import org.junit.Test;
+
+/**
+ * 100. 相同的树
+ * https://leetcode.cn/problems/same-tree/description/?show=1
+ * https://labuladong.online/algo/essential-technique/binary-tree-summary/
+ */
+public class L100SameTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+     *
+     * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(null == p && null == q)
+            return true;
+        if(null == p || null == q)
+            return false;
+        if(p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+
+    // 你可以这样构建一棵二叉树：
+    // 构建出来的二叉树是这样的：
+//      1
+//    /  \
+//   2    3
+//  / \   / \
+// 4   7  5   6
+//      \
+//       9
+    public TreeNode createTreeNode() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(7);
+        root.left.right.right = new TreeNode(9);
+
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        return root;
+    }
+
+    public TreeNode createTreeNode2() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(7);
+        root.left.right.right = new TreeNode(9);
+
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        return root;
+    }
+
+
+    @Test
+    public void test() {
+        TreeNode treeNode = createTreeNode();
+        TreeNode treeNode2 = createTreeNode2();
+        boolean result = isSameTree(treeNode, treeNode2);
+        System.out.println("result=" + result);
+    }    
+    
+
+}

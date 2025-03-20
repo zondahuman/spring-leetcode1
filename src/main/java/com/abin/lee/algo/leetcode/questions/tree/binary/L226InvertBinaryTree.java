aae80d4c98d19f818fa8341b96667a625db73e00
zondahuman <zondahuman@gmail.com>
2025-03-20 18:02:05 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/tree/binary/L226InvertBinaryTree.java
@@ -0,0 +1,75 @@
+package com.abin.lee.algo.leetcode.questions.tree.binary;
+
+import com.abin.lee.algo.util.JsonUtil;
+import org.junit.Test;
+
+/**
+ * 226. 翻转二叉树
+ * https://leetcode.cn/problems/invert-binary-tree/description/?show=1
+ * https://labuladong.online/algo/data-structure/binary-tree-part1/
+ */
+public class L226InvertBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    void traverse(TreeNode root) {
+        if (null == root)
+            return;
+        TreeNode temp1 = root.left;
+        root.left = root.right;
+        root.right = temp1;
+        traverse(root.left);
+        traverse(root.right);
+    }
+
+    // 你可以这样构建一棵二叉树：
+    // 构建出来的二叉树是这样的：
+//      1
+//    /  \
+//   2    3
+//  / \   / \
+// 4   7  5   6
+//      \
+//       9
+    public TreeNode createTreeNode() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(7);
+        root.left.right.right = new TreeNode(9);
+
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        return root;
+    }
+
+
+    @Test
+    public void test() {
+        TreeNode treeNode = createTreeNode();
+        TreeNode result = invertTree(treeNode);
+        System.out.println("result=" + JsonUtil.toJson(result));
+//        Integer result2 = invertTree(treeNode);
+//        System.out.println("result2=" + result2);
+    }
+
+
+}

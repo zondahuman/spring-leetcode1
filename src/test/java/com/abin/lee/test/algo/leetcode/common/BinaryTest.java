f4f519eb5d028c2d3dc4b1270f9d13eba43e7d3d
zondahuman <zondahuman@gmail.com>
2025-03-25 10:54:37 +0800

    add

---
+++ src/test/java/com/abin/lee/test/algo/leetcode/common/BinaryTest.java
@@ -0,0 +1,22 @@
+package com.abin.lee.test.algo.leetcode.common;
+
+import com.abin.lee.algo.util.JsonUtil;
+import org.junit.Test;
+
+public class BinaryTest {
+
+    @Test
+    public void test1() {
+        int num1 = 111 ;
+        System.out.println("list-num1=" + JsonUtil.toJson(num1));
+        int num2 = 111 ;
+        System.out.println("list-num2=" + JsonUtil.toJson(num2));
+        int num3 = 111 ;
+        System.out.println("list-num2=" + JsonUtil.toJson(num2));
+    }
+
+
+    
+
+
+}

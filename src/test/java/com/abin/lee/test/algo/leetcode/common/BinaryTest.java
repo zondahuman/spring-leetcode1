0b50327cefbc616f45f36681a8022f4e668cb4f8
zondahuman <zondahuman@gmail.com>
2025-03-25 12:40:25 +0800

    add

--- src/test/java/com/abin/lee/test/algo/leetcode/common/BinaryTest.java
+++ src/test/java/com/abin/lee/test/algo/leetcode/common/BinaryTest.java
@@ -7,16 +7,27 @@ public class BinaryTest {
 
     @Test
     public void test1() {
-        int num1 = 111 ;
+        int num1 = 1 ;
         System.out.println("list-num1=" + JsonUtil.toJson(num1));
-        int num2 = 111 ;
-        System.out.println("list-num2=" + JsonUtil.toJson(num2));
-        int num3 = 111 ;
-        System.out.println("list-num2=" + JsonUtil.toJson(num2));
+        num1 = num1 << 1;
+        System.out.println("num1 << 1-num1=" + JsonUtil.toJson(num1));
+        int num2 = num1 | 0 ;
+        System.out.println("num1 | 0-num2=" + JsonUtil.toJson(num2));
+        int num3 = num1 | 1 ;
+        System.out.println("num1 | 1-num3=" + JsonUtil.toJson(num3));
+        int num4 = num1 << 1 | 0 ;
+        System.out.println("num1 << 1 | 0-num4=" + JsonUtil.toJson(num4));
+    }
+
+    @Test
+    public void test2() {
+        int num1 = 1 ;
+        System.out.println("list-num1=" + JsonUtil.toJson(num1));
+        num1 = num1 << 1;
+        System.out.println(" num1 >> 1-num1=" + JsonUtil.toJson(num1));
     }
 
 
-    
 
 
 }

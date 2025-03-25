b73079931f5a601bbb7bb38f15d6732c9e801ba6
zondahuman <zondahuman@gmail.com>
2025-03-25 21:09:41 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/tree/binary/L103BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,86 @@
+package com.abin.lee.algo.leetcode.questions.tree.binary;
+
+import com.abin.lee.algo.util.JsonUtil;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * 103. 二叉树的锯齿形层序遍历
+ * https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/description/
+ * https://labuladong.online/algo/problem-set/binary-tree-level-i/
+ */
+public class L103BinaryTreeZigzagLevelOrderTraversal {
+    public static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        if(null == root)
+            return resultList;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean flag = false;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i <size ; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(null != node.left)
+                    queue.offer(node.left);
+                if(null != node.right)
+                    queue.offer(node.right);
+            }
+            if(flag)
+               Collections.reverse(list);
+            resultList.add(list);
+            flag = !flag;
+        }
+        return  resultList;
+    }
+
+
+    // 你可以这样构建一棵二叉树：
+    // 构建出来的二叉树是这样的：
+//      1
+//    /   \
+//   2      8
+//  / \    /  \
+// 4   5  7     9
+//      \
+//       6
+    public TreeNode createTreeNode() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.left.right.right = new TreeNode(6);
+
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        root.right.right.right = new TreeNode(10);
+        return root;
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = createTreeNode();
+        List<List<Integer>>  result = zigzagLevelOrder(root);
+        System.out.println("result=" + JsonUtil.toJson(result));
+    }
+
+
+
+}

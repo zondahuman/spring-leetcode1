e4f7ddbb3057a3f6b2ab090d98d9ec8e30f6981e
zondahuman <zondahuman@gmail.com>
2025-03-26 17:10:36 +0800

    add

--- src/main/java/com/abin/lee/algo/leetcode/questions/tree/binary/L662MaximumWidthofBinaryTree.java
+++ src/main/java/com/abin/lee/algo/leetcode/questions/tree/binary/L662MaximumWidthofBinaryTree.java
@@ -1,5 +1,8 @@
 package com.abin.lee.algo.leetcode.questions.tree.binary;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 662. 二叉树最大宽度
  * https://leetcode.cn/problems/maximum-width-of-binary-tree/description/
@@ -13,6 +16,14 @@ public class L662MaximumWidthofBinaryTree {
             this.val = val;
         }
     }
+    static class Pair{
+        TreeNode node;
+        int id;
+        public Pair(TreeNode node, int id) {
+            this.node = node;
+            this.id = id;
+        }
+    }
 
     /**
      * 给你一棵二叉树的根节点 root ，返回树的 最大宽度 。
@@ -23,8 +34,30 @@ public class L662MaximumWidthofBinaryTree {
      * @return
      */
     public int widthOfBinaryTree(TreeNode root) {
-
-        return 0;
+        if(null == root)
+            return 0;
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 1));
+        int maxWidth = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            int start = 0, end = 0 ;
+            for (int i = 0; i <size ; i++) {
+                Pair pair = queue.poll();
+                TreeNode node = pair.node;
+                int curId = pair.id;
+               if(i == 0)
+                   start = curId;
+               if(i == size -1)
+                   end = curId;
+               if(null != node.left)
+                   queue.offer(new Pair(node.left, 2*curId));
+               if(null != node.right)
+                   queue.offer(new Pair(node.right, 2*curId+1));
+            }
+            maxWidth = Math.max(maxWidth, end-start+1);
+        }
+        return maxWidth;
     }
 
 }

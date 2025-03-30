0a913fb1bd8b1ca77390f92f5cc85421e8006497
zondahuman <zondahuman@gmail.com>
2025-03-30 11:57:23 +0800

    add dp

--- src/main/java/com/abin/lee/algo/leetcode/questions/dp/L509FibonacciNumber.java
+++ src/main/java/com/abin/lee/algo/leetcode/questions/dp/L509FibonacciNumber.java
@@ -57,5 +57,18 @@ public class L509FibonacciNumber {
 
 
 
+    public int fib3(int n) {
+        if(n == 0)
+            return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1;
+        for (int i = 2; i <=n ; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+
+
 
 }

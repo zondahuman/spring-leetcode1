47845399578f7aa050f0756ef0e27b3af5dbab32
zondahuman <zondahuman@gmail.com>
2025-04-01 17:01:55 +0800

    add dp

--- src/main/java/com/abin/lee/algo/leetcode/questions/dp/L72EditDistance.java
+++ src/main/java/com/abin/lee/algo/leetcode/questions/dp/L72EditDistance.java
@@ -19,30 +19,34 @@ public class L72EditDistance {
      */
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m+1][n+1];
-        for (int i = 1; i <=m ; i++) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
             dp[i][0] = i;
         }
-        for (int j = 1; j <=n ; j++) {
+        for (int j = 1; j <= n; j++) {
             dp[0][j] = j;
         }
-        for (int i = 1; i <=m ; i++) {
-            for (int j = 1; j <=n ; j++) {
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                    dp[i][j] = min(dp[i][j-1]+1,
-                            dp[i-1][j]+1,
-                            dp[i-1][j-1]+1);
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i][j - 1] + 1,
+                            dp[i - 1][j] + 1,
+                            dp[i - 1][j - 1] + 1);
                 }
             }
         }
-        return  dp[m][n];
+        return dp[m][n];
     }
 
     public int min(int a, int b, int c){
         return Math.min(a, Math.min(b, c));
     }
+
+
+
+
 
 
 }

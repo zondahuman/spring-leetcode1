ad72e899b803328f74031e951f0b560864fd9a47
zondahuman <zondahuman@gmail.com>
2025-04-01 17:23:02 +0800

    add dp

--- src/main/java/com/abin/lee/algo/leetcode/questions/dp/L72EditDistance.java
+++ src/main/java/com/abin/lee/algo/leetcode/questions/dp/L72EditDistance.java
@@ -20,9 +20,11 @@ public class L72EditDistance {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
+        //初始化第一列。如果s2为空字符串（j=0），那么将s1[0..i-1]转换成空字符串的最小编辑距离就是将s1中的所有字符删除，即i。
         for (int i = 1; i <= m; i++) {
             dp[i][0] = i;
         }
+        //初始化第一行。同理，如果s1为空字符串（i=0），那么将空字符串转换成s2[0..j-1]的最小编辑距离就是将s2中的所有字符插入，即j。
         for (int j = 1; j <= n; j++) {
             dp[0][j] = j;
         }

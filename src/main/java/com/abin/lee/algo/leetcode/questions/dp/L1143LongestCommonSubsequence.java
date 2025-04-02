ac8a67b56b5d4bfbf60f671f20f8acfa059d5c87
zondahuman <zondahuman@gmail.com>
2025-04-02 15:30:48 +0800

    add

---
+++ src/main/java/com/abin/lee/algo/leetcode/questions/dp/L1143LongestCommonSubsequence.java
@@ -0,0 +1,51 @@
+package com.abin.lee.algo.leetcode.questions.dp;
+
+/**
+ * 1143. 最长公共子序列
+ * https://leetcode.cn/problems/longest-common-subsequence/description/
+ * https://labuladong.online/algo/dynamic-programming/longest-common-subsequence/
+ */
+public class L1143LongestCommonSubsequence {
+    /**
+     * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+     * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+     * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+     * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+     * 示例 1：
+     *
+     * 输入：text1 = "abcde", text2 = "ace"
+     * 输出：3
+     * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+     * 示例 2：
+     *
+     * 输入：text1 = "abc", text2 = "abc"
+     * 输出：3
+     * 解释：最长公共子序列是 "abc" ，它的长度为 3 。
+     * 示例 3：
+     *
+     * 输入：text1 = "abc", text2 = "def"
+     * 输出：0
+     * 解释：两个字符串没有公共子序列，返回 0 。
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n= text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i <=m ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] +1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
+
+
+}

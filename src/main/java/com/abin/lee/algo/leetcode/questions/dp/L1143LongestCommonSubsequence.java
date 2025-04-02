fa01f32a507b82ef05be150e1366a6415f7d931b
zondahuman <zondahuman@gmail.com>
2025-04-02 15:32:03 +0800

    add

--- src/main/java/com/abin/lee/algo/leetcode/questions/dp/L1143LongestCommonSubsequence.java
+++ src/main/java/com/abin/lee/algo/leetcode/questions/dp/L1143LongestCommonSubsequence.java
@@ -33,11 +33,17 @@ public class L1143LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(), n= text2.length();
         int[][] dp = new int[m+1][n+1];
+        // 定义：s1[0..i-1] 和 s2[0..j-1] 的 lcs 长度为 dp[i][j]
+        // 目标：s1[0..m-1] 和 s2[0..n-1] 的 lcs 长度，即 dp[m][n]
+        // base case: dp[0][..] = dp[..][0] = 0
         for (int i = 1; i <=m ; i++) {
             for (int j = 1; j <=n ; j++) {
+                // 现在 i 和 j 从 1 开始，所以要减一
                 if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    // s1[i-1] 和 s2[j-1] 必然在 lcs 中
                     dp[i][j] = dp[i-1][j-1] +1;
                 }else{
+                    // s1[i-1] 和 s2[j-1] 至少有一个不在 lcs 中
                     dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
             }
